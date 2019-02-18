9696e7e79637820526fb13e9d4c1e00993b6c0de
michaelhly <michaelhly@gmail.com>
2019-02-18 12:54:26 -0600

    fix bug

--- src/paginate.ts
+++ src/paginate.ts
@@ -1,6 +1,6 @@
 import { PageInfoObject } from "./types";
 
-export const paginate = <T>(collection: []): PageInfoObject => {
+export const paginate = <T>(collection: T[]): PageInfoObject => {
   const paginatedCollection = {
     cursor: collection[collection.length - 1]["id"], // tslint:disable-line:no-string-literal
     perPage: collection.length
