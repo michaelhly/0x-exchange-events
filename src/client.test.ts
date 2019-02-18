3c9a34e9312dce49a1b091d8f26fd97951820bdb
michaelhly <michaelhly@gmail.com>
2019-02-18 01:10:00 -0600

    add basic tests

--- src/client.test.ts
+++ src/client.test.ts
@@ -1,4 +1,7 @@
+import { assetDataUtils } from "@0x/order-utils";
+
 import { HttpSubgraphClient } from "./client";
+import { DEFAULT_PER_PAGE } from "./constants";
 import { Client } from "./types";
 
 describe("Tests for HttpSubgraphClient", () => {
@@ -7,11 +10,65 @@ describe("Tests for HttpSubgraphClient", () => {
     client = new HttpSubgraphClient();
   });
 
-  it("test getFilledOrdersAsync", async () => {
-    const filledOrders = await client.getFilledOrdersAsync(2, {
-      feeRecipient: "0xe269e891a2ec8585a378882ffa531141205e9"
+  it("test getFillEventsAsync, filter by feeRecipient", async () => {
+    const expectedFeeRecipient = "0x8124071f810d533ff63de61d0c98db99eeb99d64";
+    const eventLog = await client.getFillEventsAsync(DEFAULT_PER_PAGE, {
+      feeRecipient: expectedFeeRecipient
     });
-    expect(filledOrders).toBe(1);
-    // TODO: Tests
+    const { filledOrders } = eventLog.data;
+    expect(filledOrders.length).toBe(DEFAULT_PER_PAGE);
+    filledOrders.forEach(order => {
+      expect(order.feeRecipient).toBe(expectedFeeRecipient);
+    });
+  });
+
+  it("test getFillEventsAsync, filter by assetData", async () => {
+    const expectedMakerAssetData = assetDataUtils.encodeERC20AssetData(
+      "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    ); // WETH
+    const expectedTakerAssetData = assetDataUtils.encodeERC20AssetData(
+      "0xe41d2489571d322189246dafa5ebde1f4699f498"
+    ); // ZRX
+    const eventLog = await client.getFillEventsAsync(DEFAULT_PER_PAGE, {
+      makerAssetDataV2: expectedMakerAssetData,
+      takerAssetDataV2: expectedTakerAssetData
+    });
+    const { filledOrders } = eventLog.data;
+    expect(filledOrders.length).toBe(DEFAULT_PER_PAGE);
+    filledOrders.forEach(order => {
+      expect(order.makerAssetDataV2).toBe(expectedMakerAssetData);
+      expect(order.takerAssetDataV2).toBe(expectedTakerAssetData);
+    });
+  });
+
+  it("test getCancelEventAsync, filter by feeRecipient", async () => {
+    const expectedFeeRecipient = "0x8124071f810d533ff63de61d0c98db99eeb99d64";
+    const eventLog = await client.getCancelEventsAsync(DEFAULT_PER_PAGE, {
+      feeRecipient: expectedFeeRecipient
+    });
+    const { cancelledOrders } = eventLog.data;
+    expect(cancelledOrders.length).toBe(DEFAULT_PER_PAGE);
+    cancelledOrders.forEach(order => {
+      expect(order.feeRecipient).toBe(expectedFeeRecipient);
+    });
+  });
+
+  it("test getFillEventsAsync, filter by assetData", async () => {
+    const expectedMakerAssetData = assetDataUtils.encodeERC20AssetData(
+      "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    ); // WETH
+    const expectedTakerAssetData = assetDataUtils.encodeERC20AssetData(
+      "0xe41d2489571d322189246dafa5ebde1f4699f498"
+    ); // ZRX
+    const eventLog = await client.getCancelEventsAsync(DEFAULT_PER_PAGE, {
+      makerAssetDataV2: expectedMakerAssetData,
+      takerAssetDataV2: expectedTakerAssetData
+    });
+    const { cancelledOrders } = eventLog.data;
+    expect(cancelledOrders.length).toBe(DEFAULT_PER_PAGE);
+    cancelledOrders.forEach(order => {
+      expect(order.makerAssetDataV2).toBe(expectedMakerAssetData);
+      expect(order.takerAssetDataV2).toBe(expectedTakerAssetData);
+    });
   });
 });
