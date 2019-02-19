af74b09e7957ef652f099577d2d04d134a1ab8f5
michaelhly <michaelhly@gmail.com>
2019-02-18 22:27:44 -0600

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { HttpSubgraphClient } from "./client";
+export {
+  CancelEvent,
+  CancelEvents,
+  CancelEventFilter,
+  Client,
+  FillEvent,
+  FillEvents,
+  FillEventFilter
+} from "./types";
